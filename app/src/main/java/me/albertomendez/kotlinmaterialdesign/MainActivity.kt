38c88b4c5f5cce9c6e62e8ecc50e9a7d446a4d79
Alberto <alberto@mendezcabrera.com>
2018-01-11 22:10:11 +0100

    Button to change from list to grid

--- app/src/main/java/me/albertomendez/kotlinmaterialdesign/MainActivity.kt
+++ app/src/main/java/me/albertomendez/kotlinmaterialdesign/MainActivity.kt
@@ -3,6 +3,8 @@ package me.albertomendez.kotlinmaterialdesign
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.StaggeredGridLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,7 +14,11 @@ class MainActivity : AppCompatActivity() {
     lateinit private var staggeredLayoutManager: StaggeredGridLayoutManager
     lateinit private var adapter: TravelListAdapter
 
-    private val onItemClickListener = object : TravelListAdapter.OnItemClickListener{
+    lateinit private var menu: Menu
+
+    private var isListView: Boolean = false
+
+    private val onItemClickListener = object : TravelListAdapter.OnItemClickListener {
         override fun onItemClick(view: View, position: Int) {
             Toast.makeText(this@MainActivity, "Clicked: " + position, Toast.LENGTH_SHORT).show()
         }
@@ -26,5 +32,44 @@ class MainActivity : AppCompatActivity() {
         adapter = TravelListAdapter(this)
         list.adapter = adapter
         adapter.setOnItemClickListener(onItemClickListener)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        this.menu = menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_toggle) {
+            toggle()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun toggle() {
+        if (isListView) {
+            showGridView()
+        } else {
+            showListView()
+        }
+    }
+
+    private fun showListView() {
+        staggeredLayoutManager.spanCount = 1
+        val item = menu.findItem(R.id.action_toggle)
+        item.setIcon(R.drawable.ic_action_grid)
+        item.title = getString(R.string.show_as_grid)
+        isListView = true
+    }
+
+    private fun showGridView() {
+        staggeredLayoutManager.spanCount = 2
+        val item = menu.findItem(R.id.action_toggle)
+        item.setIcon(R.drawable.ic_action_list)
+        item.title = getString(R.string.show_as_list)
+        isListView = false
     }
 }
